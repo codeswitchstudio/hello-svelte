ad200c9434bf375503818c4cbf683fe311f3ca45
indira-coopts <indira.endaya@co-opts.com>
2024-12-20 14:36:14 +0000

    initialise

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 1
+
+  function increment(){
+    count++
+  }
+
+</script>
+
+<main class="flex flex-col justify-center intems-venter h-screen space-y-4">
+{count}
+<button on:click="{increment}" class="btn btn-primary">Primary</button>
+</main>
+
+<style>
+
+</style>
